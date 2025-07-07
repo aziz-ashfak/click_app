6887c17d31ceb75523b7980e8bcab9df495fc663
AzizAshfak <azizashfak@gmail.com>
2025-07-07 23:46:49 +0600

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,11 @@ import * as dotenv from 'dotenv';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
+      host: process.env.DB_HOST || 'dpg-d1lq4hqdbo4c73a7ovk0-a.oregon-postgres.render.com',
       port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
-      username: process.env.DB_USERNAME || 'postgres',
+      username: process.env.DB_USERNAME || ' mobile_attribution_app_user',
       password: process.env.DB_PASSWORD || 'aziz8313', // Default for testing
-      database: process.env.DB_DATABASE || 'postgres',
+      database: process.env.DB_DATABASE || 'mobile_attribution_app',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
