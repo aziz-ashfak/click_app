863acc7af7c8fb0e92b4e0af87f441ef10eb22ca
Aziz Ashfak <97014453+aziz-ashfak@users.noreply.github.com>
2025-07-08 00:05:01 +0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,8 @@ import * as dotenv from 'dotenv';
       type: 'postgres',
       host: process.env.DB_HOST || 'dpg-d1lq4hqdbo4c73a7ovk0-a.oregon-postgres.render.com',
       port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
-      username: process.env.DB_USERNAME || ' mobile_attribution_app_user',
-      password: process.env.DB_PASSWORD || 'aziz8313', // Default for testing
+      username: process.env.DB_USERNAME || 'mobile_attribution_app_user',
+      password: process.env.DB_PASSWORD || 'wUQjrkfnHeY4HSzOaUIDEhPyzTk4zfOl', // Default for testing
       database: process.env.DB_DATABASE || 'mobile_attribution_app',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
